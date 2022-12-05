05aef11f7236585920c546ea18046fa502148331
NataliiaKolomiichenko <gella1996nr@gmail.com>
2022-12-05 19:51:40 +0200

    add try...catch

--- contacts.js
+++ contacts.js
@@ -4,105 +4,121 @@ const path = require('node:path');
 const contactsPath = path.resolve('./db/contacts.json');
 
 async function listContacts() {
-    const contactsData = await fs.readFile(contactsPath, 'utf8', (error, data) => {
-        if (error) console.log(error);
-        return data;
-    });
-    const contacts = await JSON.parse(contactsData);
-    return Promise.all(
-        contacts.map(({id, name, email, phone}) => {
-            return {
-                Id: id,
-                Name: name,
-                Email: email,
-                Phone: phone,
-            }
-        })
-    );
+    try {
+        const contactsData = await fs.readFile(contactsPath, 'utf8', (error, data) => {
+            if (error) console.log(error);
+            return data;
+        });
+        const contacts = await JSON.parse(contactsData);
+        return Promise.all(
+            contacts.map(({id, name, email, phone}) => {
+                return {
+                    Id: id,
+                    Name: name,
+                    Email: email,
+                    Phone: phone,
+                }
+            })
+        );
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 async function getContactById(contactId) {
-    const contactsData = await fs.readFile(contactsPath, 'utf8', (error, data) => {
-        if (error) console.log(error);
-        return data;
-    });
-    const contacts = await JSON.parse(contactsData)
-    const index = contacts.findIndex(contact => contact.id === contactId.toString());
-    const {id, name, email, phone} = contacts[index];
-    return {
-        Id: id,
-        Name: name,
-        Email: email,
-        Phone: phone,
+    try {
+        const contactsData = await fs.readFile(contactsPath, 'utf8', (error, data) => {
+            if (error) console.log(error);
+            return data;
+        });
+        const contacts = await JSON.parse(contactsData)
+        const index = contacts.findIndex(contact => contact.id === contactId.toString());
+        const {id, name, email, phone} = contacts[index];
+        return {
+            Id: id,
+            Name: name,
+            Email: email,
+            Phone: phone,
+        }
+    } catch (error) {
+        console.log(error)
     }
 };
 
 async function removeContact(contactId) {
-    const contactsData = await fs.readFile(contactsPath, 'utf8', (error, data) => {
-        if (error) console.log(error);
-        return data;
-    });
+    try {
+        const contactsData = await fs.readFile(contactsPath, 'utf8', (error, data) => {
+            if (error) console.log(error);
+            return data;
+        });
 
-    const parsedContactsData = JSON.parse(contactsData)
-    
-    const index = parsedContactsData.findIndex(contact => contact.id === contactId);
-    parsedContactsData.splice(index, 1);
+        const parsedContactsData = JSON.parse(contactsData)
+        
+        const index = parsedContactsData.findIndex(contact => contact.id === contactId);
+        parsedContactsData.splice(index, 1);
 
-    const newContactsData = JSON.stringify(parsedContactsData)
+        const newContactsData = JSON.stringify(parsedContactsData)
 
-    await fs.writeFile(contactsPath, newContactsData, 'utf8', (error) => {
-        if (error) console.log(error);
-    });
+        await fs.writeFile(contactsPath, newContactsData, 'utf8', (error) => {
+            if (error) console.log(error);
+        });
 
-    const newContacts = await fs.readFile(contactsPath, 'utf8', (error, data) => {
-        if (error) console.log(error);
-        return data;
-    });
+        const newContacts = await fs.readFile(contactsPath, 'utf8', (error, data) => {
+            if (error) console.log(error);
+            return data;
+        });
 
-    const contacts = await JSON.parse(newContacts);
-    return Promise.all(
-        contacts.map(({id, name, email, phone}) => {
-            return {
-                Id: id,
-                Name: name,
-                Email: email,
-                Phone: phone,
-            }
-        })
-    );
+        const contacts = await JSON.parse(newContacts);
+        return Promise.all(
+            contacts.map(({id, name, email, phone}) => {
+                return {
+                    Id: id,
+                    Name: name,
+                    Email: email,
+                    Phone: phone,
+                }
+            })
+        );
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 async function addContact(name, email, phone) {
-    const contactsData = await fs.readFile(contactsPath, 'utf8', (error, data) => {
-        if (error) console.log(error);
-        return data;
-    });
-    
-    const parsedContactsData = JSON.parse(contactsData)
-    const newId = parsedContactsData.length+1
-    parsedContactsData.push({id: newId.toString(), name, email, phone });
-    const newContactsData = JSON.stringify(parsedContactsData)
+    try {
+        const contactsData = await fs.readFile(contactsPath, 'utf8', (error, data) => {
+            if (error) console.log(error);
+            return data;
+        });
+        
+        const parsedContactsData = JSON.parse(contactsData)
+        const newId = parsedContactsData.length+1
+        parsedContactsData.push({id: newId.toString(), name, email, phone });
+        const newContactsData = JSON.stringify(parsedContactsData)
 
-    await fs.writeFile(contactsPath, newContactsData, 'utf8', (error) => {
-        if (error) console.log(error);
-    });
+        await fs.writeFile(contactsPath, newContactsData, 'utf8', (error) => {
+            if (error) console.log(error);
+        });
 
-    const newContacts = await fs.readFile(contactsPath, 'utf8', (error, data) => {
-        if (error) console.log(error);
-        return data;
-    });
+        const newContacts = await fs.readFile(contactsPath, 'utf8', (error, data) => {
+            if (error) console.log(error);
+            return data;
+        });
 
-    const contacts = await JSON.parse(newContacts);
-    return Promise.all(
-        contacts.map(({id, name, email, phone}) => {
-            return {
-                Id: id,
-                Name: name,
-                Email: email,
-                Phone: phone,
-            }
-        })
-    );
+        const contacts = await JSON.parse(newContacts);
+        return Promise.all(
+            contacts.map(({id, name, email, phone}) => {
+                return {
+                    Id: id,
+                    Name: name,
+                    Email: email,
+                    Phone: phone,
+                }
+            })
+        );
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 module.exports = {
